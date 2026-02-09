554168f353520c32b3984d2d2c0696171b881d26
Yogaaprtma <111202113472@mhs.dinus.ac.id>
2026-02-09 20:48:48 +0700

    Perbaikan orderHistory

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -116,7 +116,10 @@ class OrderController extends Controller
 
     public function orderHistory()
     {
-        $orders = Order::where('user_id', Auth::id())->with('orderItems.product')->get();
+        $orders = Order::where('user_id', Auth::id())
+            ->with('orderItems.product')
+            ->orderBy('created_at', 'desc')
+            ->get();
         return view('customer.order.history', compact('orders'));
     }
 }
